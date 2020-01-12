7c235d4e42299e087f297d4f06b4699b06811697
Yusuke Kido <mail@yo.eki.do>
2020-01-12 21:46:05 +0900

    Version 0.0.1

---
+++ README.md
@@ -0,0 +1,27 @@
+# 29s-api
+
+Build a simple API server in less than 29 seconds.
+If you have 30 seconds, you might want to try something smarter like [typicode/json-server](https://github.com/typicode/json-server).
+
+# Usage
+
+1. Install
+   ```sh
+   npm install -S 29s-api
+   # or
+   yarn add 29s-api
+   ```
+2. Add script to package.json
+   ```js
+   {
+      // ...
+      "scripts": {
+        "start": "29s-api"
+      },
+      // ...
+   }
+   ```
+
+# Parameters
+
+See `29-api --help`
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world');
+import assert from 'assert';
+import { v2 as webdav } from 'webdav-server';
+import yargs from 'yargs';
+
+const { argv } = yargs.options({
+  port: { type: 'number', alias: 'p', default: 2929 },
+  root: { type: 'string', alias: 'r', default: '.' },
+});
+
+const server = new webdav.WebDAVServer({ port: argv.port });
+
+server.setFileSystem('/', new webdav.PhysicalFileSystem(argv.root), assert);
+
+server.start(server => {
+  assert(server);
+  console.log(`Serving ${argv.root} at port ${argv.port}`);
+});
